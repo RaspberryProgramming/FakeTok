f61476ec27b3873b5f6d46622c2780b5e89865c7
Camerin <figueroa0609@protonmail.com>
2021-08-31 14:43:34 -0400

    Fixed error with autoplay name and added loop

--- app/src/components/Video.js
+++ app/src/components/Video.js
@@ -4,7 +4,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="video">
-        <video src="/default.webm" autoplay="true"></video>
+        <video src="/default.webm" loop autoPlay></video>
       </div>
     );
   }
