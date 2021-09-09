62cd8e5c3a2084e1b3f08d5feb110ceb9a050fff
Camerin <figueroa0609@protonmail.com>
2021-09-09 15:14:31 -0400

    Modified backend to check for data store directory

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 var express = require('express');
+var fs = require('fs');
+const { nextTick } = require('process');
 
 var app = express();
 
 var tmpDatabase = {postList: ['ab45c920'], postInfo: {'ab45c920': {title: 'Switch Video', description: 'This is a test post', likes: 1, uploader: 'John Doe'}}}; // TODO: Replace with real database
 
-app.use(express.static('app/build'))
+var dataDirs = {'./dataDir': ['videos']}; // Location where video data will be stored
+
+var logLevel = "Debug"; // Set to "debug" for debug logs, undefined for no logs
+
+for (const dir in dataDirs) {
+  if (!fs.existsSync(dir)){ // Check for existance of dataDir
+    console.log(`Created datadir at: ${dir}`);
+  
+    fs.mkdirSync(dir); // Create dataDir
+  }
+
+  for (const subDir in dataDirs[dir]) {
+    let subDirPath = `${dir}/${subDir}`;
+    if (!fs.existsSync(subDirPath)){ // Check for existance of dataDir
+      console.log(`Created datadir at: ${subDirPath}`);
+    
+      fs.mkdirSync(subDirPath); // Create dataDir
+    }
+  }
+}
+
+app.use(express.static('app/build'));
+
+app.use((req, res, next) => { // Log request to terminal if loglevel = debug
+  if (logLevel === "Debug") {
+    console.log(`${req.method}: ${req.path}`);
+  }
+
+  next();
+});
 
 /* istanbul ignore next */
 if (!module.parent) {
